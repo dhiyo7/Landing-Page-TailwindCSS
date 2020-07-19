ea68c23ec7ba016a30e70d770cc0c68b8703743a
dhiyo7 <dhiyoreksa12@gmail.com>
2020-07-19 23:11:50 +0700

    22 - Bismillah & Deploy

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,7 @@ module.exports = {
       fontFamily: {
         'myfont': ['Helvetica', ...fontFamily.sans], // call this font with font-myfont
         'body': ['Brandon', ...fontFamily.sans], // call this font with font-body
+        'bodybold': ['BrandonBold', ...fontFamily.sans], // call this font with font-body
         'brandon': ['BrandonNet', ...fontFamily.sans] // call this font with font-brandon
       },
       colors: {
